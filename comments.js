62fb5a41e1964f1731e0acbda4c96e284bdb0563
Ira Baraton <rabbitshaped@gmail.com>
2026-05-20 14:11:23 +0300

    Separate comments

---
+++ comments.js
@@ -0,0 +1,34 @@
+let comments = [
+	{
+		author: "Anonymous Frog #18",
+		title: "I sat on a warm rock and now I miss it",
+		comment: `Does anyone else think about the rock they used to sit on? It was flat. It was warm. I fear I will never feel joy like that again.`,
+	},
+	{
+		author: "Old Toad",
+		title: "Toad opinion: flies are better when they struggle a little",
+		comment: `If the fly simply accepts its fate, where is the sport? I am not saying suffering is good. I am saying it adds texture.`,
+	},
+	{
+		author: "Flopsy Frog",
+		title: "I tried flying",
+		comment: `Jumped off a lily pad. Landed on my face. Gravity is a cruel mistress.`,
+	},
+	{
+		author: "Anonymous Frog #17",
+		title: "Fly philosophy",
+		comment: `Are we catching flies, or are flies choosing to be caught?`,
+	},
+	{
+		author: "Pondering Frog",
+		title: "The meaning of mud",
+		comment: `I like mud. Mud likes me. Maybe that’s all we need.`,
+	},
+	{
+		author: "Thirsty Toad",
+		title: "Morning dew",
+		comment: `Nothing beats drinking dew at sunrise. It’s like nature’s coffee.`,
+	},
+];
+
+export default comments;
